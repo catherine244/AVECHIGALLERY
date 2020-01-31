bb22fafa650922f2ad7fd3aaf2b7006ed4e27f7d
catherine244 <ccayreen24@gmail.com>
2020-01-31 12:41:38 +0300

    added location and category class in models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=30, unique=True)
+    
+    
+    def __str__(self):
+            return self.location_name
+
+
+    def save_location(self):
+        self.save()
+
+            
+class Category(models.Model):
+    cat_name = models.CharField(max_length=40, unique=True)
+    
+    
+    def __str__(self):
+        return self.cat_name
+
+
+    def save_category(self):
+        self.save()
+
+
