8d67ee80884b69b59c6a989569a6660c49cf3638
catherine244 <ccayreen24@gmail.com>
2020-01-31 14:56:55 +0300

    added search results view  on my admin app

--- gallery/views.py
+++ gallery/views.py
@@ -7,3 +7,16 @@ def welcome(request):
     category_results = Category.objects.all()
     location_results = Location.objects.all()
     return render(request,'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
+
+def search_results(request):
+    
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html',{"message":message,"all_images": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'index.html',{"message":message})
