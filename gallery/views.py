56295acfac552798b16a9903a02bd160e7163a76
catherine244 <ccayreen24@gmail.com>
2020-01-31 15:35:28 +0300

    imported image,category and location in views

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Image,Category,Location.
 # Create your views here.
 def welcome(request):
     all_images = Image.objects.all()
