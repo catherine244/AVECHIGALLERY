8165d0a5402e11556eb13a95431b30d5493a1b3b
catherine244 <ccayreen24@gmail.com>
2020-01-31 14:54:58 +0300

    added welcome request views# Create your views here. on my admin app

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+def welcome(request):
+    all_images = Image.objects.all()
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    return render(request,'index.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
