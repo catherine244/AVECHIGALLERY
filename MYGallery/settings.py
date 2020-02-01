d5828cebac6bf4db289470714d154cc0fc29ce94
catherine244 <ccayreen24@gmail.com>
2020-02-01 15:29:55 +0300

    Configured  Django App for Heroku.

--- MYGallery/settings.py
+++ MYGallery/settings.py
@@ -126,4 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
                           
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'                          
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())                          
