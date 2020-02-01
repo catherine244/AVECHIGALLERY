3d7c470f9b4250796ab703c00c4d6e1bc5039273
catherine244 <ccayreen24@gmail.com>
2020-02-01 15:28:37 +0300

    added staticfiles storage   in settings.py

--- MYGallery/settings.py
+++ MYGallery/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+                          
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'                          
