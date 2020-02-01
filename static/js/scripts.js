c1a31e391259669acea1b8617aa4a47863680631
catherine244 <ccayreen24@gmail.com>
2020-02-01 15:06:28 +0300

    added script.js  in static folder

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        helpers : {
+            title: {
+                type: 'inside',
+                position: 'top'
+            }
+        },
+            nextEffect: 'fade',
+    prevEffect: 'fade',
+        openEffect: "none",
+        closeEffect: "none",
+    });
+
+});
