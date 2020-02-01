f62412ce282077bad34d582507c944bd01ef55f1
catherine244 <ccayreen24@gmail.com>
2020-02-01 16:29:59 +0300

    created a Django admin

--- MYGallery/settings.py
+++ MYGallery/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
-                          
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 # Configure Django App for Heroku.
-django_heroku.settings(locals())                          
+django_heroku.settings(locals())
+                       
